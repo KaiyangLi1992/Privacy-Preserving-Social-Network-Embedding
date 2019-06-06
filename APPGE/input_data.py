b39ad91826bf29fe359483b6df6be1b078178104
root <kaiyang.li@outlook.com>
2019-06-06 23:33:46 +0000

    add comments

--- APPGE/input_data.py
+++ APPGE/input_data.py
@@ -7,11 +7,14 @@ import numpy as np
 
 def load_data(dataset):
     
+    # load feature matrix 
     with open('../data/{}_feats.pkl'.format(dataset), 'rb') as f1:
         features = pkl.load(f1)
+    # load adjacency matrix 
     with open('../data/{}_adj.pkl'.format(dataset), 'rb') as f2:
         adj = pkl.load(f2,encoding='latin1')
     
+    # load validation set and test set
     val_edges = np.load('../data/{}_val_edges.npy'.format(dataset))
     val_edges_false = np.load('../data/{}_val_edges_false.npy'.format(dataset))
     test_edges = np.load('../data/{}_test_edges.npy'.format(dataset))
