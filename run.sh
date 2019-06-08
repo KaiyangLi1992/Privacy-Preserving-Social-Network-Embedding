4cfc64b8781d4a555197b2223c880673223c8fc1
root <kaiyang.li@outlook.com>
2019-06-08 00:09:34 +0000

    add run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+# Embedd yale with APGE
+python ./APGE/train.py -dataset yale
+# Embedd yale with ADPGE
+#python ./ADPGE/train.py -dataset yale
+# Embedd yale with APPGE
+#python ./APPGE/train.py -dataset yale
+# Embedd rochester with APGE
+#python ./APGE/train.py -dataset rochester -epochs 1000 -hidden2 8
+# Embedd rochester with ADPGE
+#python ./ADPGE/train.py -dataset rochester -epochs 1000 -hidden2 8
+# Embedd rochester with APPGE
+#python ./APPGE/train.py -dataset rochester -epochs 5000 -hidden2 8
+
